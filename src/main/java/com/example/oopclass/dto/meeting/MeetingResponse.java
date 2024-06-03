d2d54edbd82cd55446631a759d3627182421eb86
Youjeong <yj91322@gmail.com>
2024-06-03 16:56:44 +0900

    fix: 모임 조회 반환형식 수정

--- src/main/java/com/example/oopclass/dto/meeting/MeetingResponse.java
+++ src/main/java/com/example/oopclass/dto/meeting/MeetingResponse.java
@@ -3,21 +3,32 @@ package com.example.oopclass.dto.meeting;
 import com.example.oopclass.domain.meeting.Meeting;
 import lombok.Getter;
 
+import java.util.Date;
+
 @Getter
 public class MeetingResponse {
     private String subjectName;
     private int desiredCount;
     private String teamType;
     private String username;
+    private String userId;
+    private String studentNumber;
+    private String userMajor;
     private String title;
     private String description;
+
+    private Date date;
 
     public MeetingResponse(Meeting meeting) {
         this.subjectName = meeting.getSubject().getSubjectName();
         this.desiredCount = meeting.getDesiredCount();
         this.teamType = meeting.getTeamType();
         this.username = meeting.getUser().getUsername();
+        this.userId = meeting.getUser().getUserId();
+        this.studentNumber = meeting.getUser().getStudentNumber();
+        this.userMajor = meeting.getUser().getMainMajor().getMajorName();
         this.title = meeting.getTitle();
         this.description = meeting.getDescription();
+        this.date = meeting.getUpdatedAt();
     }
 }
